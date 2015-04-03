e1a7fb4c2566718af4272c18a3903b4f68a7b3c7
lollin48 <elise@dixon.net>
2015-04-03 11:56:37 -0400

    Dropdown menu
    Changed to a drop down menu.
    lots of javascript was added to create the dropdown, first to enable
    the dropdown menu, and then to call the different tabs.
    
    The html was changed to become a dropdown, by creating a single li,
    which has another ul nested inside of it. The ul has the links needed,
    noted as a “#” followed by a, b, or c.
    
    The css changed to make the text saying “dropdown” black when hovered
    over by the mouse.

--- Javascript/general.js
+++ Javascript/general.js
@@ -1,11 +1,10 @@
-var main = function() {
-    $(".col-sm-4").height($(".col-sm-8").height());   
-};
-
-$(document).ready(main);
-
-$(body).scrollspy({target: 'navbar-example'});
-
-$('[data-spy="scroll"]').each(function () {
-  var $spy = $(this).scrollspy('refresh')
-});
+$(document).ready(
+  $(".dropdown a").click(function(e) {
+      e.preventDefault()
+      $(this).tab('show')
+  }
+  $('.dropdown a[href="#a"]').tab('show') 
+  $('.dropdown a[href="#b"]').tab('show') 
+  $('.dropdown a[href="#c"]').tab('show') 
+// Select tab by name 
+);
